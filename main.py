159727a6fcf1d7f7786531e1145c7c21f4c69d84
Jatinrajpal <jatinrajpal57@gmail.com>
2021-04-18 13:41:40 +0530

    update

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect, request
+
 app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+	return render_template("home.html",title="home")
+
+
+@app.route("/register")	
+def register():
+    return render_template("register.html",title="register")
+    		
 @app.route("/about")
 def about():
     return "<h1>About Page</h1>"
